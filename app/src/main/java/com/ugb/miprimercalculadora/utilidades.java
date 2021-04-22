670d422f591ad06a50bf59498e2f31386e151f14
Elmer Antonio Angel Reyes <69090593+ElmerAngel-ugb@users.noreply.github.com>
2021-04-21 21:35:45 -0600

    Creacion de metodo utilidades donde se almacena la URL y la IP de la pc que puede ser modificada y creacion de metodos de conexion

---
+++ app/src/main/java/com/ugb/miprimercalculadora/utilidades.java
@@ -0,0 +1,6 @@
+package com.ugb.miprimercalculadora;
+
+public class utilidades {
+    public static final String urlServer = "http://192.168.0.110:5984/db_productos/_design/producto/_view/mi-producto";
+
+}
