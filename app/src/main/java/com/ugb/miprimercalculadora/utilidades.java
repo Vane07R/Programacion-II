1b5ef396ae87b34c7fa39ecdde17e89d5692fb68
Roger Zelaya <rogerzelayaaaa@gmail.com>
2021-04-23 15:56:32 -0600

    se agrego obtener, mostrar productos online, ofline.

---
+++ app/src/main/java/com/ugb/miprimercalculadora/utilidades.java
@@ -0,0 +1,6 @@
+package com.ugb.miprimercalculadora;
+
+public class utilidades {
+    static String url_consulta = "http://192.168.1.4:5984/db_agenda/_design/agenda/_view/mi-agenda";
+    static String url_mto = "http://192.168.1.4:5984/db_agenda/";
+}
