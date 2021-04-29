765e71037ce2fc79c28b716b40eebb84a96d9d02
Roger Zelaya <rogerzelayaaaa@gmail.com>
2021-04-29 14:23:37 -0600

    muchas cosas

--- app/src/main/java/com/ugb/miprimercalculadora/utilidades.java
+++ app/src/main/java/com/ugb/miprimercalculadora/utilidades.java
@@ -1,7 +1,7 @@
 package com.ugb.miprimercalculadora;
 
 public class utilidades {
-    static String url_consulta = "http://192.168.0.138:5984/db_agenda/_design/tienda/_view/db_tienda";
-    static String url_mto = "http://192.168.0.138:5984/db_agenda/";
+    static String url_consulta = "http://192.168.1.5:5984/de_pelicula/_design/peliculas/_view/de_peliculas";
+    static String url_mto = "http://192.168.1.5:5984/de_peliculas";
 }
 
