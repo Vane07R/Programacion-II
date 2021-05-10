dc2a0c195e5e627ef47a4129fd21dc459ae9bfee
Usuario <usis011120@ugb.edu.sv>
2021-05-10 16:51:05 -0600

    Clase DB

--- app/src/main/java/com/ugb/miprimercalculadora/DB.java
+++ app/src/main/java/com/ugb/miprimercalculadora/DB.java
@@ -1,4 +1,24 @@
 package com.ugb.miprimercalculadora;
 
-public class DB {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DB extends SQLiteOpenHelper{
+    static String nombreDB ="db_restaurante";
+    static String tblrestaurante="CREATE TABLE tblrestaurante()";//capos de la tabla de la base de datos
+    public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, nombreDB, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
