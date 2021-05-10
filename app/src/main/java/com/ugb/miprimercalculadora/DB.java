465ad1f406f95ee05ed62a80a81d1a142a174796
Usuario <usis011120@ugb.edu.sv>
2021-05-10 17:01:10 -0600

    Clase DB

--- app/src/main/java/com/ugb/miprimercalculadora/DB.java
+++ app/src/main/java/com/ugb/miprimercalculadora/DB.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 
 public class DB extends SQLiteOpenHelper{
     static String nombreDB ="db_restaurante";
-    static String tblrestaurante="CREATE TABLE tblrestaurante()";//capos de la tabla de la base de datos
+    static String tblrestaurante="CREATE TABLE tblrestaurante()";//campos de la tabla de la base de datos
     public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, nombreDB, factory, version);
     }
